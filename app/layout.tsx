b95cf8e22388e7745dcd902d4f83eb1dae7f66a0
Ryan Berke <ryanberke@Ryans-MacBook-Pro.local>
2025-07-12 21:51:58 -0400

    fixed fr this time

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { Roboto } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const roboto = Roboto({
   weight: ['300', '400', '500', '700', '900'],
@@ -32,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${roboto.variable} antialiased font-roboto`}
+        className={`${roboto.variable} antialiased font-roboto`}
       >
         {children}
       </body>
